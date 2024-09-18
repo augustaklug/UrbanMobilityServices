00086d3cecf39290eeefc8da7fdb51cc845b9141
Augusta Carla Klug <augustaklug@gmail.com>
2024-09-18 14:59:16 -0300

    Docker compose

--- mobility-recommendation-service/src/main/resources/db/migration/V1__Create_mobility_recommendation_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS mobility_recommendation (
-    id SERIAL PRIMARY KEY,
-    zip_code VARCHAR(8) NOT NULL,
-    recommended_mode VARCHAR(255) NOT NULL,
-    timestamp TIMESTAMP NOT NULL
-);
